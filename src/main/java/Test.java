704193aa2b1e9258fedff2bad9c3e718368dd329
Rajesh <rajesh.daswani@gmail.com>
2025-06-09 21:09:23 +0100

    further updates to Test.java

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,7 +1,34 @@
+import java.util.Scanner;
+
 public class Test {
 
     public static void main (String [] args){
-        String name = "Ram";
-        System.out.println(name.charAt(0));
+        double principalAmount;
+        double interest;
+        double time;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter the principal amount ");
+        principalAmount=scanner.nextDouble();
+
+        System.out.println("Please enter the interest ");
+        interest=scanner.nextDouble();
+
+        System.out.println("Please enter the time in years ");
+        time=scanner.nextDouble();
+
+        double simpleInterest = (principalAmount*interest*time)/100;
+
+        System.out.printf("The simple interest is $%.3f",simpleInterest);
+
+
+
+
+
+
+
+        scanner.close();
+
     }
 }
